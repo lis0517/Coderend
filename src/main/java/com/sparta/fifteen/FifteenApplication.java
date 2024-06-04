1d169beadbba100f13e60c8d90b03b0efbfd3412
조현아 <minjokwindows5@gmail.com>
2024-06-04 11:42:01 +0900

    Initial commit

---
+++ src/main/java/com/sparta/fifteen/FifteenApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.fifteen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifteenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FifteenApplication.class, args);
+    }
+
+}
