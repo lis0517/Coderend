08237f1d53681726166f91175146f1a74a003145
HongYongGuen <163097270+HongYongGuen@users.noreply.github.com>
2024-06-05 18:53:00 +0900

    profile 기능 구현 완료

--- src/main/java/com/sparta/fifteen/controller/ProfileController.java
+++ src/main/java/com/sparta/fifteen/controller/ProfileController.java
@@ -8,9 +8,11 @@ import com.sparta.fifteen.service.ProfileService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.webjars.NotFoundException;
 
 @RestController
 @RequestMapping("/api/user")
@@ -26,6 +28,7 @@ public class ProfileController {
     @PutMapping("/profile")
     public ResponseEntity<String> updateProfile(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody ProfileRequestDto ProfileRequestDto) {
 
+        HttpHeaders headers = new HttpHeaders();
 
         try {
             User user = userDetails.getUser();
@@ -34,8 +37,15 @@ public class ProfileController {
 
             return ResponseEntity.ok().body("프로필 수정에 성공하였습니다.");
 
+        } catch (NotFoundException e) {
+            headers.add("Message", "해당 사용자를 찾을 수 없습니다.");
+            return new ResponseEntity<>(null, headers, HttpStatus.NOT_FOUND); // 사용자를 찾을 수 없는 경우에는 NOT_FOUND(404) 상태를 반환합니다.
+        } catch (AccessDeniedException e ) {
+            headers.add("Message", "인증되지 않은 사용자입니다.");
+            return new ResponseEntity<>(null, headers, HttpStatus.UNAUTHORIZED); // 인증되지 않은 경우에는 UNAUTHORIZED(401) 상태를 반환합니다.
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body("프로필 수정에 실패하였습니다.");
+            headers.add("Message", "프로필 수정 중에 오류가 발생했습니다.");
+            return new ResponseEntity<>(null, headers, HttpStatus.INTERNAL_SERVER_ERROR); // 예외가 발생했을 때는 INTERNAL_SERVER_ERROR(500) 상태를 반환합니다.
         }
 
     }
@@ -52,18 +62,26 @@ public class ProfileController {
             ProfileResponseDto userProfile = profileService.getUserProfile(user);
 
             if (userProfile == null) {
-                headers.add("Message", "프로필 조회에 실패하였습니다.");
-                return new ResponseEntity<>(null, headers, HttpStatus.BAD_REQUEST);
+                headers.add("Message", "프로필이 존재하지 않습니다.");
+                return new ResponseEntity<>(null, headers, HttpStatus.NOT_FOUND);
             }
 
             headers.add("Message", "프로필 조회에 성공하셨습니다.");
             return new ResponseEntity<>(userProfile, headers, HttpStatus.OK);
 
+        }  catch (NotFoundException e) {
+             headers.add("Message", "해당 사용자를 찾을 수 없습니다.");
+             return new ResponseEntity<>(null, headers, HttpStatus.NOT_FOUND); // 사용자를 찾을 수 없는 경우에는 NOT_FOUND(404) 상태를 반환합니다.
+        } catch (AccessDeniedException e ) {
+             headers.add("Message", "인증되지 않은 사용자입니다.");
+             return new ResponseEntity<>(null, headers, HttpStatus.UNAUTHORIZED); // 인증되지 않은 경우에는 UNAUTHORIZED(401) 상태를 반환합니다.
         } catch (Exception e) {
-            headers.add("Message", "프로필 조회에 실패하였습니다.");
-            return new ResponseEntity<>(null, headers, HttpStatus.BAD_REQUEST);
+             headers.add("Message", "프로필 조회 중에 오류가 발생했습니다.");
+            return new ResponseEntity<>(null, headers, HttpStatus.INTERNAL_SERVER_ERROR); // 예외가 발생했을 때는 INTERNAL_SERVER_ERROR(500) 상태를 반환합니다.
         }
     }
+
+
 
 
 }
--- src/main/java/com/sparta/fifteen/service/ProfileService.java
+++ src/main/java/com/sparta/fifteen/service/ProfileService.java
@@ -3,10 +3,13 @@ package com.sparta.fifteen.service;
 import com.sparta.fifteen.dto.ProfileRequestDto;
 import com.sparta.fifteen.dto.ProfileResponseDto;
 import com.sparta.fifteen.entity.User;
+import com.sparta.fifteen.error.PasswordMismatchException;
 import com.sparta.fifteen.repository.UserRepository;
 import lombok.Setter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.InputMismatchException;
 
 @Service
 public class ProfileService {
@@ -20,8 +23,11 @@ public class ProfileService {
 
     public void updateProfile(User user, ProfileRequestDto profileRequestDto) {
 
+        if(!profileRequestDto.isPasswordMatching()){
+            throw new PasswordMismatchException("새로운 패스워드와 현재 비밀번호가 동일합니다.");
+        }
         if(!profileRequestDto.isNewPasswordMatch()) {
-            return;
+            throw new PasswordMismatchException("새로운 패스워드와 확인 패스워다가 일치 하지 않습니다.");
         }
 
         user.updateProfile(profileRequestDto);
@@ -31,12 +37,8 @@ public class ProfileService {
     @Transactional
     public  ProfileResponseDto getUserProfile(User user) {
 
-        if (user == null) {
-            return null;
-        }
 
         return new ProfileResponseDto(user);
     }
-
 
 }
