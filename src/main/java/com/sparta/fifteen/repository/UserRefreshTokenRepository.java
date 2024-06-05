eb53f744614a852437f151150f80ce988f810171
lis0517 <01034895017a@gmail.com>
2024-06-05 11:22:15 +0900

    rename RefreshToken, RefreshTokenRepository
    앞에 User 제거

--- src/main/java/com/sparta/fifteen/repository/UserRefreshTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.fifteen.repository;
-
-import com.sparta.fifteen.entity.UserRefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRefreshTokenRepository extends JpaRepository<UserRefreshToken, Long> {
-}
