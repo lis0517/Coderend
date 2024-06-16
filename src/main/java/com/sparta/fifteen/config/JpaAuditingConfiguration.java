c2ecf8c22c06208a4e81a6e0d889e4bc6400e944
lis0517 <01034895017a@gmail.com>
2024-06-16 11:28:15 +0900

    fix Auditing

---
+++ src/main/java/com/sparta/fifteen/config/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package com.sparta.fifteen.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+}
